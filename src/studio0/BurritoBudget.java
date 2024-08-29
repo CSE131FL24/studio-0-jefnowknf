37cc6e99ae8203c34c811e71a2c1bf59531f6e90
dhruv <dhruv@DhruvDell.dhcp.wustl.edu>
2024-08-29 12:37:59 -0500

    commit project changes

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -8,7 +8,8 @@ public class BurritoBudget {
 		int burritosEatenWeek3 = 19; //don't judge me
 		int burritosEatenWeek4 = 1;
 		
-		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
+		double totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
+		System.out.println(totalBurritos);
 		double averageWeeklyBurritos = totalBurritos / 4;
 		
 		double burritoPrice = 7.50;
